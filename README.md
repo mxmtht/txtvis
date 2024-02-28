2932a496719cd5f44a0e2243537718e2c56a5526
Max Mathot <maximilian@mathot-berlin.de>
2024-02-28 17:34:20 +0100

    Implement txtvis script

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # txtvis
 A simple python script that takes raw text input, extracts entities using spaCy NER and draws a network based on the input content.
+This version of the script only works with Dutch input text.
+
+## Setup
+
+After cloning into the repository, first install the required packages into a virtual environment.
+```
+cd to/this/repo/
+mkdir out
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Now go ahead and add some source material to the `in` directory.
+The naming convention for the input texts is as follows:
+```
+John Doe.txt
+<First Name> <Last Name>.txt
+```
+You are ready to generate network data.
+
+## Generate network data
+
+Do not forget to remove the example data from the `in/` directory.
+To execute the script run the following command:
+```
+python3 main.py
+```
+This will generate a `.gexf` file in the `out` directory, which can be edited with [Gephi](https://gephi.org).
+Do not forget to look into the main.py source code file to check for options, like generating a pyvis-html-file for interactive exploration of your network data.
+
+## Handy stuff
+
+Some nice things, that can help you debug and understand what is happening.
+
+### Display the entities in a text
+Open a python console with in this venv `source venv/bin/activate` and then `python`.
+
+```
+import spacy
+from spacy import displacy
+nlp = spacy.load("nl_core_news_md")
+doc = nlp(open('in/Pietertje Janssen.txt').read())
+displacy.serve(doc, style='ent', port=5002, host='127.0.0.1')
+```
---
+++ main.py
@@ -0,0 +1,101 @@
+import glob
+import spacy
+import networkx as nx
+from pyvis.network import Network
+
+nlp = spacy.load("nl_core_news_md")
+
+pyvis_mode = False
+
+# networkX things
+G = nx.MultiDiGraph()
+
+# coloring (only for pyvis)
+color_mapping = {
+    "NORP": "red",
+    "LANGUAGE": "blue",
+    "LOC": "green",
+    "GPE": "pink",
+    "ORG": "purple",
+    "PERSON": "yellow",
+    "EVENT": "brown",
+    "FAC": "orange",
+    "BOOKAUTHOR": "gray",
+}
+
+# entity label as category
+categories = {}
+
+# input texts
+path = 'in/*.txt'
+
+# Entity exclusion settings
+excluded_labels = ['DATE', 'PERCENT', 'CARDINAL', 'ORDINAL', 'WORK_OF_ART']
+excluded_pos = ['ADJ', 'ADV', 'VERB']
+excluded_lemmas = []
+
+
+def author_to_entity_network(author, doc):
+    author_lastname = "".join(author.split(" ")[-1:])
+    print(f"### {author_lastname} ###")
+    categories[author_lastname] = "BOOKAUTHOR"
+    for entity in doc.ents:
+        if entity.label_ not in excluded_labels \
+                and doc[entity.start].pos_ not in excluded_pos \
+                and entity.text not in excluded_lemmas \
+                and len(entity.text) > 2 \
+                and not entity.text.__contains__(']') \
+                and not entity.text.__contains__('*') \
+                and not entity.text.__contains__('\\'):
+            print(entity.text, entity.label_, entity.ent_id, doc[entity.start].pos_)
+            if entity.label_ == "PERSON":  # persons are identified by their last names
+                ent_lastname = " ".join(entity.text.split(" ")[-1:])
+                if ent_lastname != author_lastname:
+                    G.add_edge(author_lastname, ent_lastname)
+                    categories[entity.text] = entity.label_
+            else:
+                G.add_edge(author_lastname, entity.text)
+                categories[entity.text] = entity.label_
+
+
+def strip_firstname(ent_list):
+    ent_str_list = []
+    for ent in ent_list:
+        if ent.label_ == "PERSON":
+            ent_lastname = " ".join(ent.text.split(" ")[-1:])
+            ent_str_list.append(ent_lastname)
+        else:
+            ent_str_list.append(ent.text)
+    return ent_str_list
+
+
+def pyvis_network():
+    # calculate node sizes (based on in_degrees)
+    note_degrees = dict(G.degree)
+    nx.set_node_attributes(G, note_degrees, 'size')
+
+    # set category based on entity type
+    nx.set_node_attributes(G, categories, 'category')
+
+    net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
+    net.force_atlas_2based(gravity=-300, spring_length=100, spring_strength=0.12, central_gravity=0.05, overlap=0.25)
+    net.from_nx(G)
+    for node in net.nodes:
+        try:
+            category = node['category']
+        except:
+            category = 'default'
+        color = color_mapping.get(category, "gray")  # Default to gray if the category is not in the mapping
+        node["color"] = color
+    net.show("out/network.html", notebook=False)
+
+if __name__ == "__main__":
+    docs = {}
+    for file in glob.glob(path):
+        with open(file, encoding='utf-8', errors='ignore') as file_in:
+            text = file_in.read()
+            doc = nlp(text)
+            author_to_entity_network(file[6:-4], doc)
+    if pyvis_mode:
+        pyvis_network()
+    nx.write_gexf(G, "out/network.gexf")
